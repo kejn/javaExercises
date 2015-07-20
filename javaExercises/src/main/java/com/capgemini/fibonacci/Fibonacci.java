e27bb1da9ee9c56193bca1aa802c9a3bbb23caa2
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-07-20 13:47:05 +0200

    test fib.

--- javaExercises/src/main/java/com/capgemini/fibonacci/Fibonacci.java
+++ javaExercises/src/main/java/com/capgemini/fibonacci/Fibonacci.java
@@ -1,6 +1,6 @@
 package com.capgemini.fibonacci;
 
-/**
+/** 
  * Created by ldrygala on 2015-01-23.<br>
  * F1	F2	F3	F4	F5	F6	F7	F8	F9	F10	F11	F12	F13	F14	F15	F16	F17	  F18	F19<br>
  * 1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987	1597  2584	4181
